e21cc6c12407a789000ee46f5bc521c485bf914e
Hostagen <68284806+Hostagen@users.noreply.github.com>
2021-05-28 17:38:50 +0900

    Initial Type Button

--- discord_components/__init__.py
+++ discord_components/__init__.py
@@ -1,2 +1,4 @@
 __title__ = "components"
 __version__ = '0.1.0'
+
+from .create import *
--- discord_components/create.py
+++ discord_components/create.py
@@ -1,12 +1,47 @@
+from typing import Union
+
+class ComponentType:
+
+    @property
+    def action_row(self):
+        return 1
+
+    @property
+    def button(self):
+        return 2
+
+
+class ButtonStyle:
+
+    @property
+    def primary(self):
+        return 1
+
+    @property
+    def secondary(self):
+        return 2
+
+    @property
+    def success(self):
+        return 3
+
+    @property
+    def danger(self):
+        return 4
+
+    @property
+    def link(self):
+        return 5
+
 
 def button(
-        button_type: int,
-        style,
-        label,
+        button_type: Union[int, ComponentType],
+        style: Union[int, ButtonStyle],
+        label: str,
         emoji,
-        custom_id,
-        url,
-        disabled: bool
+        custom_id: str,
+        url: str,
+        disabled: bool = False
 ) -> dict:
     data = {
         "type": button_type,
